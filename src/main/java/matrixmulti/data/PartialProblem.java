b927256576f4c572c8b08ada800d30e09d603de1
Felix Range <felix.range.f28@fh-zwickau.de>
2018-06-25 10:56:43 +0200

    added serialization for partialProblem

--- src/main/java/matrixmulti/data/PartialProblem.java
+++ src/main/java/matrixmulti/data/PartialProblem.java
@@ -1,5 +1,7 @@
 package matrixmulti.data;
 
+import java.util.Arrays;
+
 public class PartialProblem {
 	private final double[] values;
 	private final int row;
@@ -21,6 +23,40 @@ public class PartialProblem {
 
 	public int getColumn() {
 		return column;
+	}
+	
+	/**
+	 * Serialize the PartialProblem
+	 * @return A string in the format values#row#column
+	 */
+	public String serialize() {
+		String s = "";
+		s += Arrays.toString(getValues());
+		s += "#";
+		s += Integer.toString(getRow());
+		s += "#";
+		s += Integer.toString(getColumn());
+		return s;
+	}
+	
+	/**
+	 * Deserialize a partial problem
+	 * @param s the serialized string
+	 * @return a partial problem conatining the serialized values.
+	 * @throws Exception when the string is not in the correct format
+	 */
+	public static PartialProblem deserialize(String s) throws Exception {
+		String[] params = s.split("#");
+		if (params.length == 3) {
+			String[] stringifiedValues = params[0].replace("[", "").replaceAll("]", "").split(", ");
+			double[] values = new double[stringifiedValues.length];
+			for (int i = 0; i < stringifiedValues.length; i++) {
+				values[i] = Double.parseDouble(stringifiedValues[i]);
+			}
+			int row = Integer.parseInt(params[1]);
+			int column = Integer.parseInt(params[2]);
+			return new PartialProblem(values, row, column);
+		} else throw new Exception("Invalid String format. Cannot parse to partial Problem");
 	}
 	
 }
